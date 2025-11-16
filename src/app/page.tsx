0224ad76717da99e4aea3dc2e33cdbedc75435bb
Kenny Diaz <kdiaz7543@gmail.com>
2025-11-16 00:34:48 +0000

    ahora a verde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold tracking-tighter text-destructive">
+      <h1 className="text-6xl font-bold tracking-tighter text-green-500">
         Hola Mundo
       </h1>
     </main>
