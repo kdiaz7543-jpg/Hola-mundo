5f8fcbf8287943520d7abf4360abcac91d94231d
Kenny Diaz <kdiaz7543@gmail.com>
2025-11-16 00:32:40 +0000

    cambia el textoa color rojo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold tracking-tighter text-primary">
+      <h1 className="text-6xl font-bold tracking-tighter text-destructive">
         Hola Mundo
       </h1>
     </main>
